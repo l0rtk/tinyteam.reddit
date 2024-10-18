825a22a8b6ce89af903cfe51b8232a18f41a0040
luka <edulukalortk@gmail.com>
2024-10-18 18:14:46 +0000

    initial data fetcher function

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 # Initialize Reddit API client
 reddit = praw.Reddit(
-    client_id=os.getenv('REDDIT_CLIENT_ID'),
-    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-    user_agent=os.getenv('REDDIT_USER_AGENT')
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    user_agent=os.getenv('USER_AGENT')
 )
 
 def fetch_reddit_posts(keyword, limit=10):
